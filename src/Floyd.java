be5957b46551a8889dbc250d62c3f6121d2a4ad3
PG_13 <740894912@qq.com>
2017-10-10 14:37:29 +0800

    add .classpath into repo

---
+++ src/Floyd.java
@@ -0,0 +1,243 @@
+import java.util.Random;
+
+public class Floyd {
+	private String [] str = {"to", "explore", "strange", "new", "worlds" ,"seek" ,"out" ,"life" ,"and" , "civilizations" };
+	private int n = str.length;
+	private String [] str_0 = new String[n];
+	private int[][] in_matrix = new int [n][n];
+	private int [][]out_matrix = new int [n][n];
+	private int [] vis = {0,1,2,3,4,5,6,7,8,9};
+	String s1;
+	
+	void turn()
+	//void turn (String [] str ,int[][] in_matrix,int[][] out_matrix,int [] vis)
+	 {
+			int k=0;
+			/**for(int i=0;i<str.length;i++)
+			{
+				for(int j=0;j<i;j++)
+				{
+					if(str[i].compareTo(str[j])==0)
+					{
+						vis[i]=vis[j];
+					}
+					else
+					{
+						vis[i]=k;k++;
+					}
+				}			
+			}**/
+			k=0;
+			for(int i=0;i<str.length;i++)
+			{
+				if(vis[i]==k) 
+				{
+					str_0[k]=str[i];
+					k++;
+				}
+			}
+			
+			
+			for(int i=0;i<str.length-1;i++)
+			{
+				in_matrix[vis[i]][vis[i+1]]++;
+				out_matrix[vis[i+1]][vis[i]]++;
+			}
+			in_matrix[0][5] = 1;
+		    in_matrix[4][0] = 1;
+		    in_matrix[6][3] = 1;
+		    in_matrix[3][7] = 1;
+		    in_matrix[8][3] = 1;
+		    in_matrix[3][9] = 1;
+		    in_matrix[4][5] = 0;
+		    in_matrix[6][7] = 0;
+		    in_matrix[8][9] = 0;
+    }
+	
+    
+	String calcShortestPath(String word1,String word2) {  
+		 int m1 = 0, m2 = 0;   
+		 int n = in_matrix.length; 
+		    int[][] D = new int[n][n];//保存从i到j的最小路径值  
+		    int[][] p = new int[n][n];//保存经过的中间节点  
+		    // 找到对应序号
+		    for(int i=0;i<str.length;i++)
+			 {
+				 String str0 =new String(str[i]);
+				 if(str0.compareTo(word1)==0) m1=vis[i]; //找到初始起点
+				 if(str0.compareTo(word2)==0) m2=vis[i]; //找到结尾节点
+			 }
+		    
+		    for (int i = 0; i < n; i++) {//初始化D，p  
+		        for (int j = 0; j < n; j++) {  
+		            if (in_matrix[i][j] != 0) {  
+		                p[i][j] = j;  
+		            } else {  
+		                p[i][j] = -1;  
+		            }  
+		            if (in_matrix[i][j]!=0)
+		            {	
+		            	D[i][j] = in_matrix[i][j];  
+		            }
+		            else
+		            {
+		            	D[i][j] = 100;
+		            }
+		        }  
+		    }  
+		 
+		    for (int x = 0; x < n; x++) {//进行Floyd算法，从0到n-1所有可能进行遍历  
+		        for (int i = 0; i < n; i++) {  
+		            for (int j = 0; j < n; j++) {  
+		                if (D[i][j] > D[i][x] + D[x][j]) {  
+		                    D[i][j] = D[i][x] + D[x][j];  
+		                    p[i][j] = p[i][x];  
+		                }  
+		            }  
+		        }  
+		    }  
+		    // 下面对获得的结果进行展示  
+
+		            //System.out.println("输出" + i + "到" + j + "最短路径：");  
+		   String strN = word1;
+          int k = p[m1][m2];  
+		   if (k == -1) {  
+		     System.out.println("没有最短路径");  
+		   } else {  
+		                //System.out.print(" " + k);  
+		       String str0 = str_0[k];
+		       strN = strN + "->" + str0;
+		       while (k != m2) {  
+		           k = p[k][m2];  
+		           str0 = str_0[k];
+		           strN = strN + "->" + str0;  
+		       }  
+
+		                //System.out.println(strN); 
+		    } 
+	        return strN;
+     }
+	String calcShortestPath(String word1) {  
+		 int m1 = 0, m2 = 0;   
+		 int n = in_matrix.length; 
+		 String strr = "";
+		    int[][] D = new int[n][n];//保存从i到j的最小路径值  
+		    int[][] p = new int[n][n];//保存经过的中间节点  
+		    // 找到对应序号
+		    for(int i=0;i<str.length;i++)
+			 {
+				 String str0 =new String(str[i]);
+				 if(str0.compareTo(word1)==0) m1=vis[i]; //找到初始起点
+			 }
+		    
+		    for (int i = 0; i < n; i++) {//初始化D，p  
+		        for (int j = 0; j < n; j++) {  
+		            if (in_matrix[i][j] != 0) {  
+		                p[i][j] = j;  
+		            } else {  
+		                p[i][j] = -1;  
+		            }  
+		            if (in_matrix[i][j]!=0)
+		            {	
+		            	D[i][j] = in_matrix[i][j];  
+		            }
+		            else
+		            {
+		            	D[i][j] = 100;
+		            }
+		        }  
+		    }  
+		 
+		    for (int x = 0; x < n; x++) {//进行Floyd算法，从0到n-1所有可能进行遍历  
+		        for (int i = 0; i < n; i++) {  
+		            for (int j = 0; j < n; j++) {  
+		                if (D[i][j] > D[i][x] + D[x][j]) {  
+		                    D[i][j] = D[i][x] + D[x][j];  
+		                    p[i][j] = p[i][x];  
+		                }  
+		            }  
+		        }  
+		    }  
+		    // 下面对获得的结果进行展示  
+		    String strN = word1;
+		    for(m2 = 0;m2 <n;m2++)
+	           {        
+		    	   if (m2 != m1)
+		    	   {strr = strr + "输出" + word1 + "到" + str_0[m2] + "的最短路径：\r\n" ;  
+			       int k = p[m1][m2];  
+			       if (k == -1) 
+			       {  
+			            strr = strr + "没有最短路径\r\n";  
+			       }   
+			       else {  
+			                //System.out.print(" " + k);  
+			       String str0 = str_0[k];
+			       strN = strN + "->" + str0;
+			       while (k != m2) {  
+			           k = p[k][m2];  
+			           str0 = str_0[k];
+			           strN = strN + "->" + str0;  
+			       }  
+
+			    strr = strr + strN + "\r\n";
+			    strN = word1;
+			    } }
+	           }
+	      return strr;
+
+     }
+	String randomWalk()
+	{
+        int n = str.length;
+        int str_len = str.length;
+		int [][] cp_matrix = new int[n][n];
+		String strr;
+		for(int i = 0;i<n;i++)
+		{
+			for (int j = 0;j<n;j++)
+			{
+				cp_matrix[i][j] = in_matrix[i][j];
+			}
+		}
+		Random rand = new Random();
+		int j = 5;
+		//int j = rand.nextInt(str_len)%(str_len+1);
+		strr = str[j];
+		boolean flag =true ;
+		while(flag)
+		{
+			for(int k=0;k<str_len;k++)
+			{
+				if (cp_matrix[j][k]!= 0)
+				{
+					if (cp_matrix[j][k] == 1)
+					{strr = strr+ " "+ str[k];
+					cp_matrix[j][k]= -1;
+					j = k;
+					k = -1;}
+					else
+					{
+						strr = strr+ " "+ str[k];
+						break;
+					}
+				}
+			}
+			flag = false;
+		}
+		return strr;
+	}
+	
+	public static void main(String[] args) {
+		Floyd c = new Floyd();
+		c.turn();
+		String s = c.randomWalk();
+		System.out.println(s);
+		/**System.out.println(s);
+		System.out.println();
+		s = c.calcShortestPath("new", "seek");
+		System.out.println(s);
+		s = c.calcShortestPath("new");
+		System.out.println(s);**/
+	}
+	  
+}
---
+++ src/Lab1.java
@@ -0,0 +1,127 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javax.swing.*;
+
+public class Lab1 extends JComponent {
+	private static final long serialVersionUID = -4654513992552014113L;
+	public static frame f;
+	public static String fileUrl;
+	public static String[] words;
+	
+	public static void readInFile(){
+		File file = new File(fileUrl);
+		String wordsStr = "";
+		Scanner in;
+		try {
+			in = new Scanner(file);
+			while(in.hasNextLine()){
+				String str = in.nextLine();
+				wordsStr = wordsStr.concat(replaceStr(str));
+			}
+			words = wordSplit(wordsStr);
+			createFlowChartFrame();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+		// GradientPaint grdp = new GradientPaint(150, 150, Color.decode("#81FBB8"), 300, 300, Color.decode("#28C76F"));
+		// g2d.setPaint(grdp);
+		// g2d.fillRoundRect(150, 150, 150, 150, 15, 15);
+		int len = 0;
+		for (int i = 0; i< words.length;i++)
+		{    
+			g2d.drawString(words[i], 10+len*7, 10);
+			len = len + words[i].length()+1;
+		}
+	}
+	
+	public Lab1() {
+		setBackground(Color.WHITE);
+	}
+	
+	public static void createFlowChartFrame() {
+		final JFrame f = new JFrame("流程图");  
+        // f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        f.setSize(800, 600);  
+        f.add(new Lab1());  
+        f.setVisible(true);  
+        f.setLocationRelativeTo(f.getOwner());  
+	}
+	
+	/** 切割字符串
+	 * @param str 读入的字符串
+	 * @return 切割后的String[] \\s+
+	 */
+	public static String[] wordSplit(String str) {
+		return str.split("\\s+");
+	}
+	
+	/** 处理读入的字符串(删除标点符号，并转换为小写)
+	 * @param str 读入的字符串
+	 * @return 处理后的字符串
+	 */
+	public static String replaceStr(String str){
+		return str.replaceAll("[^a-zA-Z]", " ").toLowerCase();
+	}
+	
+	public static void main(String[] args) {
+		f = new frame();
+	}
+}
+
+class frame extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6904245993409935448L;
+	private static final int WIDTH = 220;
+	private static final int HEIGHT = 250;
+	private static panel p;
+	
+	
+	public frame() {
+		setTitle("Test Frame");
+		setSize(WIDTH, HEIGHT);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		Container c = new Container();
+		c = getContentPane();
+		p = new panel();
+		c.add(p);
+		setVisible(true);
+	}
+}
+
+class panel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -466385864846654643L;
+	
+	public panel(){
+		JButton btnOpen = new JButton("Open");
+		btnOpen.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				FileDialog fd = new FileDialog(Lab1.f, "Choose file", FileDialog.LOAD);
+				fd.setFile("*.txt");
+				fd.setVisible(true);
+				String filename = fd.getFile();
+				if (filename != null){
+					Lab1.fileUrl = fd.getDirectory() + fd.getFile();
+					Lab1.readInFile();
+				}
+			}
+		});
+		add(btnOpen);
+	}
+}
